1135a93d4b19d13ee2ff5ff59ebefaba81875f9c
adelinejune <adelinejunee@gmail.com>
2023-03-24 14:48:07 +0800

    [+] add cek resi page and controller

--- lib/app/modules/resi/view.dart
+++ lib/app/modules/resi/view.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ongkos/app/modules/resi/controller.dart';
 
-class CekResi extends StatelessWidget {
-  const CekResi({super.key});
+class CekResiPage extends GetView<ResiController> {
+  const CekResiPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("CekResi")),
+      body: Column(children: [
+        Obx(() => Text("Counter ${controller.counter}")),
+        ElevatedButton(
+            onPressed: () {
+              controller.increaseCounter();
+            },
+            child: Text('Increase'))
+      ]),
     );
   }
 }
