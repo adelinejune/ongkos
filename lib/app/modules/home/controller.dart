392e74612b5a0faae724fedfb81e556816c8a599
adelinejune <adelinejunee@gmail.com>
2023-03-27 17:02:55 +0800

    [+] add city list to home controller

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -1,5 +1,28 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:ongkos/app/data/models/city.dart';
+import 'package:ongkos/app/data/services/repository.dart';
 
 class HomeController extends GetxController {
+  CityRepository cityRepository;
+  HomeController({required this.cityRepository});
+
   final String title = 'Home Title';
+  RxList cities = [].obs;
+
+  @override
+  void onInit() async {
+    // TODO: implement onInit
+    super.onInit();
+    try {
+      await cityRepository.fetchCities();
+      var citiesList = cityRepository.cities();
+      cities = RxList.from(citiesList.map((item) => item.toJson()));
+      print(cities);
+    } catch (error) {
+      print("Error fetching cities: $error");
+    }
+
+    print("done");
+  }
 }
--- lib/app/modules/navigation/binding.dart
+++ lib/app/modules/navigation/binding.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 import 'package:ongkos/app/modules/home/controller.dart';
 import 'package:ongkos/app/modules/navigation/controller.dart';
 import 'package:ongkos/app/modules/resi/controller.dart';
+import 'package:ongkos/app/data/services/repository.dart';
+import 'package:ongkos/app/data/providers/city/provider.dart';
 
 class NavigationBinding extends Bindings {
   @override
@@ -10,7 +12,8 @@ class NavigationBinding extends Bindings {
       () => NavigationController(),
     );
     Get.lazyPut<HomeController>(
-      () => HomeController(),
+      () => HomeController(
+          cityRepository: CityRepository(cityProvider: CityProvider())),
     );
     Get.lazyPut<ResiController>(
       () => ResiController(),
