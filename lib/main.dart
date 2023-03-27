6222549abebec184fc49eb56c6750b2e18012f91
adelinejune <adelinejunee@gmail.com>
2023-03-27 17:01:37 +0800

    [+] configure dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:ongkos/app/core/theme/theme_service.dart';
@@ -7,6 +8,8 @@ import 'package:ongkos/app/routes/app_pages.dart';
 import 'package:ongkos/app/routes/app_routes.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env');
+
   await GetStorage.init();
   runApp(const MyApp());
 }
