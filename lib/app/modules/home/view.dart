9e21e2051c648277896eb750eabf99254872ed67
adelinejune <adelinejunee@gmail.com>
2023-03-31 17:10:03 +0800

    [+] add kurir choicechip to UI

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -10,192 +10,259 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
+    var squareWidth = Get.width - 15.0.wp;
+
     return Scaffold(
-        body: SafeArea(
-      child: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 3.5.wp),
-            child: SvgPicture.asset(
-              Get.isDarkMode
-                  ? 'assets/images/logo_dark.svg'
-                  : 'assets/images/logo_light.svg',
-              fit: BoxFit.contain,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 5.0.wp, vertical: 5.0.wp),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColorDark,
-                borderRadius: BorderRadius.circular(10),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 3.5.wp),
+              child: SvgPicture.asset(
+                Get.isDarkMode
+                    ? 'assets/images/logo_dark.svg'
+                    : 'assets/images/logo_light.svg',
+                fit: BoxFit.contain,
               ),
-              child: Padding(
-                padding: EdgeInsets.all(4.5.wp),
-                child: Row(
-                  children: [
-                    SvgPicture.asset(
-                      Get.isDarkMode
-                          ? 'assets/images/home_arrow_dark.svg'
-                          : 'assets/images/home_arrow_light.svg',
-                    ),
-                    const SizedBox(
-                      width: 10.0,
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              Get.to(() => PilihKota(tipe: 'asal'));
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                side: BorderSide(color: Colors.grey),
-                              ),
-                            ),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(left: 0),
-                                  child: Obx(
-                                    () => controller.kotaAsal.value.isEmpty
-                                        ? Text(
-                                            'Kota Asal',
-                                            style: TextStyle(
-                                                color: Colors.black45,
-                                                fontSize: 14),
-                                          )
-                                        : Text(
-                                            controller.kotaAsal.value,
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 15),
-                                          ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20.0,
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              Get.to(() => PilihKota(tipe: 'tujuan'));
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                side: BorderSide(color: Colors.grey),
-                              ),
-                            ),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(left: 0),
-                                  child: Obx(
-                                    () => controller.kotaTujuan.value.isEmpty
-                                        ? Text(
-                                            'Kota Tujuan',
-                                            style: TextStyle(
-                                                color: Colors.black45,
-                                                fontSize: 14),
-                                          )
-                                        : Text(
-                                            controller.kotaTujuan.value,
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 15),
-                                          ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+            ),
+
+            // Pemilihan Kota Asal dan Tujuan
+            Padding(
+              padding:
+                  EdgeInsets.symmetric(horizontal: 5.0.wp, vertical: 5.0.wp),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColorDark,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(4.5.wp),
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        Get.isDarkMode
+                            ? 'assets/images/home_arrow_dark.svg'
+                            : 'assets/images/home_arrow_light.svg',
                       ),
-                    ),
-                  ],
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Get.to(() => PilihKota(tipe: 'asal'));
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  side: BorderSide(color: Colors.grey),
+                                ),
+                              ),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 0),
+                                    child: Obx(
+                                      () => controller.kotaAsal.value.isEmpty
+                                          ? Text(
+                                              'Kota Asal',
+                                              style: TextStyle(
+                                                  color: Colors.black45,
+                                                  fontSize: 14),
+                                            )
+                                          : Text(
+                                              controller.kotaAsal.value,
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 15),
+                                            ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20.0,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                Get.to(() => PilihKota(tipe: 'tujuan'));
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  side: BorderSide(color: Colors.grey),
+                                ),
+                              ),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 0),
+                                    child: Obx(
+                                      () => controller.kotaTujuan.value.isEmpty
+                                          ? Text(
+                                              'Kota Tujuan',
+                                              style: TextStyle(
+                                                  color: Colors.black45,
+                                                  fontSize: 14),
+                                            )
+                                          : Text(
+                                              controller.kotaTujuan.value,
+                                              style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 15),
+                                            ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-              padding:
-                  EdgeInsets.symmetric(horizontal: 5.0.wp, vertical: 2.0.wp),
+
+            // Pemilihan Berat
+            Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: 5.0.wp, vertical: 2.0.wp),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Berat Paket',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Container(
+                      height: 40,
+                      child: TextField(
+                        autocorrect: false,
+                        autofocus: false,
+                        controller: controller.beratController,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Work Sans',
+                          fontWeight: FontWeight.w500,
+                        ),
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10.0,
+                            horizontal: 10.0,
+                          ),
+                          suffixIcon: Container(
+                            width: 30,
+                            child: const Center(
+                              child: Text(
+                                'kg',
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontFamily: 'Work Sans',
+                                    fontSize: 15),
+                              ),
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: BorderSide(
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                        onChanged: (value) => controller.assignBerat(value),
+                      ),
+                    )
+                  ],
+                )),
+
+            //Pemilihan Kurir
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 5.0.wp, right: 5.0.wp, top: 4.0.wp, bottom: 2.0.wp),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Berat Paket',
+                    'Pilih Kurir',
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 12.0,
                   ),
-                  Container(
-                    height: 40,
-                    child: TextField(
-                      autocorrect: false,
-                      autofocus: false,
-                      controller: controller.beratController,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Work Sans',
-                        fontWeight: FontWeight.w500,
-                      ),
-                      keyboardType:
-                          TextInputType.numberWithOptions(decimal: true),
-                      decoration: InputDecoration(
-                        contentPadding: const EdgeInsets.symmetric(
-                          vertical: 10.0,
-                          horizontal: 10.0,
-                        ),
-                        suffixIcon: Container(
-                          width: 30,
-                          child: Center(
-                            child: Text(
-                              'kg',
-                              style: TextStyle(
-                                  color: Colors.grey,
-                                  fontFamily: 'Work Sans',
-                                  fontSize: 15),
-                            ),
-                          ),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(
-                            color: Colors.grey.shade300,
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(
-                            color: Colors.grey.shade300,
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                          borderSide: BorderSide(
-                            color: Colors.grey.shade300,
-                          ),
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                      ),
-                      onChanged: (value) => controller.assignBerat(value),
-                    ),
+                  Wrap(
+                    spacing: 2.0.wp,
+                    children: controller.kurir
+                        .map((e) => Obx(() {
+                              return SizedBox(
+                                width: squareWidth / 3,
+                                child: ChoiceChip(
+                                  label: Container(
+                                    width: squareWidth / 3,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: AssetImage(e['image']!),
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                  ),
+                                  selected: controller.selectedKurir
+                                      .contains(e['code']),
+                                  backgroundColor: Colors.white,
+                                  selectedColor: Colors.grey.shade300,
+                                  pressElevation: 0,
+                                  onSelected: (selected) {
+                                    if (selected) {
+                                      controller.selectedKurir.add(e['code']);
+                                    } else {
+                                      controller.selectedKurir
+                                          .remove(e['code']);
+                                    }
+                                    print(controller.selectedKurir);
+                                  },
+                                  shape: ContinuousRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    side: BorderSide(
+                                        color: Colors.transparent, width: 0.5),
+                                  ),
+                                  //
+                                ),
+                              );
+                            }))
+                        .toList(),
                   )
                 ],
-              ))
-        ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
